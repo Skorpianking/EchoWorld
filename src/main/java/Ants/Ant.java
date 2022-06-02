0c5df278dd9ac53242159778b5d0b982fdb6fe76
David King <Davidking.jr@gmail.com>
2022-06-01 20:32:13 -0400

    Add files via upload

--- src/main/java/Ants/Ant.java
+++ src/main/java/Ants/Ant.java
@@ -26,6 +26,7 @@ public class Ant extends SimulationBody {
     String offenseTag;
     String defenseTag;
     String matingTag;
+    String parent = "NONE";
 
     // Parameter for seeing how long the ant lived, they can recharge at home.  More successful ants should live longer
     // Generation = timestep it was introduced into the population
@@ -47,11 +48,10 @@ public class Ant extends SimulationBody {
     private Vector2 home; // where the ant's home is
 
     // Actual ant "body" for the world to paint
-    private SimulationBody basicAnt;
     private Vector2 leftWheelLocation = new Vector2(-0.5, -0.5); // holdovers from vehicle class, right now
     private Vector2 rightWheelLocation = new Vector2( 0.5, -0.5); // I don't want to rehash the physics to turn the ant
 
-    // All taken from the cue basicAnt class template
+    // All taken from the cue class template
     // 2.25 in diameter = 0.028575 m radius
     final double ballRadius = 0.25; //0.1; //0.028575;
     // 0.126 oz/in^3 = 217.97925 kg/m^3
@@ -65,34 +65,30 @@ public class Ant extends SimulationBody {
     private final double ANGULAR_DAMPENING = 0.1;
     private final double K_p = 10;   //Proportional Control Constant
 
-
     private int SENSOR_RANGE = 2; // how far an ant can see
     private int MAX_TORQUE = 1; // how fast we can turn
     private double GRAB_RANGE = 1; // how far "off" the target can be, allows us to home in on a target
 
     // World the ant lives in
-    World<SimulationBody> myWorld;
+    protected World<SimulationBody> myWorld;
 
-    public Ant(World<SimulationBody> world) {
-        myWorld = world;
-        basicAnt = new SimulationBody(new Color(255, 255, 255)); // leaving this in, ideally i'd like child ants to be a combination of parent colors
-        basicAnt.setColor(Color.CYAN);
-        basicAnt.addFixture(Geometry.createCircle(ballRadius), ballDensity, ballFriction, ballRestitution);
-        basicAnt.translate(-0.25, 0.0);
+    public Ant(World<SimulationBody> myWorld) {
+        this.myWorld = myWorld;
+        //this.setColor(Color.CYAN);
+        setRandomColor();
+        this.addFixture(Geometry.createCircle(ballRadius), ballDensity, ballFriction, ballRestitution);
         this.setRandomVelocity(); // set a random initial velocity
-        basicAnt.setLinearDamping(0.3);
-        basicAnt.setAngularDamping(0.8);
-        basicAnt.setMass(MassType.NORMAL);
-        myWorld.addBody(basicAnt);
+        this.setLinearDamping(0.3);
+        this.setAngularDamping(0.8);
+        this.setMass(MassType.NORMAL);
 
         int max = 15;
         int min = -15;
         double x_pos = Math.floor(Math.random()*(max-min+1)+min); // done just for code clarity
         double y_pos = Math.floor(Math.random()*(max-min+1)+min);
-        basicAnt.translate(x_pos,y_pos);
+        this.translate(x_pos,y_pos);
 
-        home = new Vector2(-15,0); // center of the screen for now.
-        //            g.fillRect((int)(point.x*scale-r*05),(int)(point.y*scale-r*.05),3,3);
+        home = new Vector2(x_pos,y_pos); // center of the screen for now.
 
         // Instantiate behaviorTree
         ArrayList<Double> weights = new ArrayList<>();
@@ -105,8 +101,8 @@ public class Ant extends SimulationBody {
         behaviorTree.add(new GotoXX("Home"));
         behaviorTree.add(new GotoXX("Resource"));
         setInitialTag(); // set the ant's tag
-        id = this.matingTag; //+" "+basicAnt.getWorldCenter().toString(); // hopefully this makes their names random enough for now
-
+        id = matingTag+offenseTag+defenseTag; // combine all the tags eventually
+        tag = matingTag+offenseTag+defenseTag;
     }
 
     /**
@@ -125,7 +121,6 @@ public class Ant extends SimulationBody {
      */
     public Ant(Ant copy) {
         this.myWorld = copy.myWorld;
-        this.basicAnt = copy.basicAnt;
         this.id = copy.id;
         this.home = copy.home;
         this.state = copy.state;
@@ -133,7 +128,6 @@ public class Ant extends SimulationBody {
         this.behaviorTree = copy.behaviorTree;
         this.alive = copy.alive;
         this.lifespan = copy.lifespan;
-        this.myWorld.addBody(this.basicAnt);
         this.tag = copy.tag;
         this.offenseTag = copy.offenseTag;
         this.defenseTag = copy.defenseTag;
@@ -142,14 +136,12 @@ public class Ant extends SimulationBody {
         this.generation = copy.generation;
         this.life = copy.life;
         this.death = copy.death;
+        this.color = copy.color;
     }
 
-    /**
-     *
-     */
     public boolean sense() {
         state.tick();
-        state.setVelocity(basicAnt.getLinearVelocity()); // LinearVelocity captures heading and speed
+        state.setVelocity(this.getLinearVelocity()); // LinearVelocity captures heading and speed
         return true;
     }
 
@@ -172,6 +164,7 @@ public class Ant extends SimulationBody {
      * @param a Action being applied to the vehicle
      */
     public void act(Action a) {
+
         double left = a.getLeftWheelVelocity();
         double right = a.getRightWheelVelocity();
 
@@ -188,27 +181,19 @@ public class Ant extends SimulationBody {
         double baseTorque = torqueL + torqueR;
 
         // Proportional Controller
-        double error = baseTorque - this.basicAnt.getAngularVelocity(); // SetPoint - ProcessVariable (e(t) = r(t)-y(t))
+        double error = baseTorque - this.getAngularVelocity(); // SetPoint - ProcessVariable (e(t) = r(t)-y(t))
         double u = K_p * error; // Control variable
 
         // Apply Torque
-        this.basicAnt.applyTorque(u);
+        this.applyTorque(u);
 
         // System.out.println("Current: " + baseVehicle.getAngularVelocity() + "; Applied: " + u + "; Target: " + baseTorque);
 
         // Apply the forces in the direction the baseVehicle is facing
-        Vector2 baseNormal = this.basicAnt.getTransform().getTransformedR(new Vector2(0.0,1.0));
+        Vector2 baseNormal = this.getTransform().getTransformedR(new Vector2(0.0,1.0));
         baseNormal.multiply(left+right);
-        this.basicAnt.setLinearVelocity(baseNormal);
-    }
+        this.setLinearVelocity(baseNormal);
 
-    /**
-     * Set the color of the vehicle
-     *
-     * @param c Color
-     */
-    public void setColor(Color c) {
-        basicAnt.setColor(c);
     }
 
     /**
@@ -309,12 +294,11 @@ public class Ant extends SimulationBody {
      * Determines if the ant is "home." If so, it's lifespan is extended. -- This will change in the future.
      */
     private void updateHome() {
-        double dist = this.basicAnt.getWorldCenter().distance(home);
+        double dist = this.getWorldCenter().distance(home);
         if(dist < 2) {
             //System.out.println("Dist from home: " + dist + " WC: " + this.basicAnt.getWorldCenter());
             lifespan = 2000;
             setRandomVelocity(); // send it back into the world
-            this.basicAnt.setColor(Color.CYAN);
         }
     }
 
@@ -323,10 +307,10 @@ public class Ant extends SimulationBody {
      * to dropping off resources at home for other ants to use.
      */
     private void updateLife() {
-        if(lifespan <= 100*basicAnt.getWorldCenter().distance(home)) {
-            Vector2 heading = new Vector2(basicAnt.getWorldCenter(), home);
-            double angle = heading.getAngleBetween(basicAnt.getLinearVelocity()); // radians
-            double distance = basicAnt.getWorldCenter().distance(home); //
+        if(lifespan <= 100*this.getWorldCenter().distance(home)) {
+            Vector2 heading = new Vector2(this.getWorldCenter(), home);
+            double angle = heading.getAngleBetween(this.getLinearVelocity()); // radians
+            double distance = this.getWorldCenter().distance(home); //
             SensedObject obj = new SensedObject(heading, angle, distance, "Home", "None", home);
             state.addSensedObject(obj);
         }
@@ -341,12 +325,12 @@ public class Ant extends SimulationBody {
      */
     private void updateAnts(ArrayList<Ant> allAgent_ants) {
         for(Ant antObj : allAgent_ants) {
-            Vector2 heading = new Vector2(basicAnt.getWorldCenter(), antObj.basicAnt.getWorldCenter());
-            double angle = heading.getAngleBetween(basicAnt.getLinearVelocity()); // radians
-            double distance = basicAnt.getWorldCenter().distance(antObj.basicAnt.getWorldCenter()); //
+            Vector2 heading = new Vector2(this.getWorldCenter(), antObj.getWorldCenter());
+            double angle = heading.getAngleBetween(this.getLinearVelocity()); // radians
+            double distance = this.getWorldCenter().distance(antObj.getWorldCenter()); //
             if(distance <= SENSOR_RANGE && this.id != antObj.id) { // ignore oneself
                 String type = "Ant";
-                SensedObject obj = new SensedObject(heading, angle, distance, type, "None", antObj.basicAnt.getWorldCenter());
+                SensedObject obj = new SensedObject(heading, angle, distance, type, "None", antObj.getWorldCenter());
                 state.addSensedObject(obj);
                 //System.out.println("I see an ant: " + antObj.id);
 
@@ -404,9 +388,9 @@ public class Ant extends SimulationBody {
 
         // Find the closest resource and head towards it
         for(Resource resObj : resources) {
-            Vector2 heading = new Vector2(basicAnt.getWorldCenter(), resObj.location);
-            double angle = heading.getAngleBetween(basicAnt.getLinearVelocity()); // radians
-            double tDist = basicAnt.getWorldCenter().distance(resObj.location); //
+            Vector2 heading = new Vector2(this.getWorldCenter(), resObj.location);
+            double angle = heading.getAngleBetween(this.getLinearVelocity()); // radians
+            double tDist = this.getWorldCenter().distance(resObj.location); //
             if(tDist < distance && tDist <= SENSOR_RANGE) {
                 String type = "Resource";
                 distance = tDist;
@@ -427,17 +411,23 @@ public class Ant extends SimulationBody {
     }
 
     /**
-     * Sets the basicAnts linear velocity to a random vector
+     * Sets the linear velocity to a random vector
      */
     private void setRandomVelocity() {
         Random rand = new Random();
         int max = 2;
         int min = -2;
-        basicAnt.setLinearVelocity(rand.nextInt((max - min) + 1) + min,rand.nextInt((max - min) + 1) + min);
+        this.setLinearVelocity(rand.nextInt((max - min) + 1) + min,rand.nextInt((max - min) + 1) + min);
+    }
+
+    private void setRandomColor() {
+        Random rand = new Random();
+        Color c = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+        this.setColor(c);
     }
 
     public void render(Graphics2D g, double scale) {
-        super.render(g, scale, Color.CYAN);
+        super.render(g, scale);
     }
 
     public boolean isAlive() {
@@ -471,16 +461,12 @@ public class Ant extends SimulationBody {
         }
     }
 
+    public void setParent(String par) {
+        parent = par;
+    }
+
     public void incLife() {
         this.lifespan++;
-    }
-
-    public SimulationBody getBasicAnt() {
-        return basicAnt;
-    }
-
-    public void setBasicAnt(SimulationBody basicAnt) {
-        this.basicAnt = basicAnt;
     }
 
     public int getGeneration() {
